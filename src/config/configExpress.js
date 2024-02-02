1569a2ece58904f27dc4675e4141bacbab1e588f
Stefan367 <stefan.ivanov673@gmail.com>
2024-02-02 11:55:33 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 function configExpress(app) {
     app.use(express.static(path.resolve('src/public')));
+    app.use(express.urlencoded({ extended: false })); // urlencoded - ако има подадени данни от клиента тази функция ще ги парсне за нас автоматично
 
     return app;
 }
