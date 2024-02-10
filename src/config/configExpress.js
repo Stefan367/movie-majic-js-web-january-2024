fe26bbb83b8599f1031e12264eb4c2764a7dedae
Stefan367 <stefan.ivanov673@gmail.com>
2024-02-10 18:25:10 +0200

    Add cookie-parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 function configExpress(app) {
     app.use(express.static(path.resolve('src/public')));
     app.use(express.urlencoded({ extended: false })); // urlencoded - ако има подадени данни от клиента тази функция ще ги парсне към обект за нас автоматично
+    app.use(cookieParser());
 
     return app;
 }
