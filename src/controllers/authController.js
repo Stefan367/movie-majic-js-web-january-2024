64e92d8cf76b7a931bf1292bca168a13e0c0c00f
Stefan367 <stefan.ivanov673@gmail.com>
2024-02-10 11:07:11 +0200

    add register page

---
+++ src/controllers/authController.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const authService = require('../services/authService');
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    await authService.register(userData);
+
+    res.redirect('/login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+module.exports = router;
