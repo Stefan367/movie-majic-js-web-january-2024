777371130fd68713e238f1fbc41e7865eda8f720
Stefan367 <stefan.ivanov673@gmail.com>
2024-02-10 18:24:51 +0200

    Add json web token when login

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, MongooseError } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
@@ -6,12 +6,26 @@ const userSchema = new Schema({
         type: String,
         required: true,
         lowercase: true, 
+        unique: true,
     },
     password: {
         type: String,
         required: true,
     },
 });
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 12);
+
+    this.password = hash;
+});
+
+userSchema.virtual('rePassword')
+    .set(function(value) {
+        if (value !== this.password) {
+            throw new MongooseError('Password mismatch!');
+        }
+    });
 
 const User = model('User', userSchema);
 
