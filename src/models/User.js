2f584a25c91de6f8b08a1b4e888b356f350ab88b
Stefan367 <stefan.ivanov673@gmail.com>
2024-02-12 00:39:12 +0200

    Throw error when user already exists

--- src/models/User.js
+++ src/models/User.js
@@ -7,10 +7,14 @@ const userSchema = new Schema({
         required: true,
         lowercase: true, 
         unique: true,
+        match: [/@\w+\.\w+$/, 'Invalid email address'],
+        minLength: [10, 'Email should be at least 10 characters long'],
     },
     password: {
         type: String,
         required: true,
+        match: [/^[a-zA-Z0-9]+$/, 'Password should be alphanumeric'],
+        minLength: 6,
     },
 });
 
